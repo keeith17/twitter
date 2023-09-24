9f85922d8cb07a3b04b9d64c565df2a9a5b5311b
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2023-09-24 23:59:18 +0900

    레이아웃 1차 완성 / firebase auth 기본 세팅

--- react-twitter/src/App.tsx
+++ react-twitter/src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Layout } from 'components/Layout';
-import Router from 'components/Router';
+import { useState } from "react";
+
+import { Layout } from "components/Layout";
+import Router from "components/Router";
+
+import { getAuth } from "firebase/auth";
+import { app } from "firebaseApp";
 
 function App() {
-  return (
-    <Layout>
-      <Router />
-    </Layout>
-  );
+    const auth = getAuth(app);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+        !!auth?.currentUser
+    );
+    console.log(auth, isAuthenticated);
+    return (
+        <Layout>
+            <Router />
+        </Layout>
+    );
 }
 
 export default App;
