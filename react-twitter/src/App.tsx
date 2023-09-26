818787393faa103f4ceedc078996a2c8abf78461
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2023-09-26 14:52:24 +0900

    firebase 세팅 2

--- react-twitter/src/App.tsx
+++ react-twitter/src/App.tsx
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Layout } from "components/Layout";
 import Router from "components/Router";
 
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
     const auth = getAuth(app);
+    const [init, setInit] = useState<boolean>(false);
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
         !!auth?.currentUser
     );
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setIsAuthenticated(true);
+            } else {
+                setIsAuthenticated(false);
+            }
+            setInit(true);
+        });
+    }, [auth]);
     console.log(auth, isAuthenticated);
     return (
         <Layout>
-            <Router />
+            <ToastContainer />
+            {init ? <Router isAuthenticated={isAuthenticated} /> : "loading"}
         </Layout>
     );
 }
